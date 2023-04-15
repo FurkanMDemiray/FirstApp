ee0498f0ee83eaf259c7c35b4f26612a366c3d08
Furkan Melik Demiray <109747603+FurkanMDemiray@users.noreply.github.com>
2023-04-16 01:23:06 +0300

    Added git ignore

--- app/src/main/java/com/example/kentsimgeleri/MainActivity.kt
+++ app/src/main/java/com/example/kentsimgeleri/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.kentsimgeleri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kentsimgeleri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // add binding to the activity
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // initialize binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // set the content view to the binding root
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.hello.text = "Hello World! I am Kotlin"
     }
+
 }
