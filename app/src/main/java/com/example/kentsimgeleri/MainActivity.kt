35b05070b84d59393405382fdda5fae8dbaaf444
Furkan Melik Demiray <109747603+FurkanMDemiray@users.noreply.github.com>
2023-04-16 01:19:01 +0300

    First Commit

---
+++ app/src/main/java/com/example/kentsimgeleri/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kentsimgeleri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // add binding to the activity
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
